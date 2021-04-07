46d4fc46428ef5a0d4ea1d01770153894a8e6c11
Akhil Namboodiri <akhil.namboodiri@somaiya.edu>
2021-04-07 18:42:25 +0530

    mongodb credentials changed

--- server/keys.js
+++ server/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGOURI: 'mongodb+srv://Akhil:lLJdWKLQPFmM2gj6@cluster0.w3r4f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  MONGOURI: 'mongodb+srv://studiare:GMTxmBweZDhIHm1l@cluster0.mvr1l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   JWT_SECRET: 'miniproject',
   API_KEY: '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',
 };
