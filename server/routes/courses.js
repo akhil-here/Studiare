11c048fb4270456465c67814e0fd6035ad80d066
Akhil Namboodiri <akhil.namboodiri@somaiya.edu>
2021-04-08 23:35:55 +0530

    api done

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -1,14 +1,16 @@
 const express = require ('express');
 const router = express.Router ();
 const mongoose = require ('mongoose');
+const creatingCourse = require ('../middleware/creatingCourse');
 const Courses = mongoose.model ('Courses');
-const requireLogin = require ('../middleware/requireLogin');
+// const requireLogin = require ('../middleware/requireLogin');
 
-router.post ('/createcourse', requireLogin, (req, res) => {
+router.post ('/createcourse', creatingCourse, (req, res) => {
   const {course_name, price} = req.body;
   if (!course_name || !price) {
     return res.status (422).json ({error: 'Please add all the fields!!'});
   }
+  req.user.password = undefined;
   const course = new Courses ({
     course_name,
     price,
